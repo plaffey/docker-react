54cdfda8b1a31ae9198eb3915dd5b53bb5927343
Peter Laffey <peter@brainspace.com>
2020-08-15 21:47:51 -0500

    chagned app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Again...
+          Change to Feature
         </a>
       </header>
     </div>
