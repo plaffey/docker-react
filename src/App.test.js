27d5a6cd02e0334b0de9e84a9b6b9340e08a06f7
Peter Laffey <peter@brainspace.com>
2020-08-15 21:57:42 -0500

    test fix

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Change to Feature.../i);
+  const linkElement = getByText(/Change to Feature/i);
   expect(linkElement).toBeInTheDocument();
 });
